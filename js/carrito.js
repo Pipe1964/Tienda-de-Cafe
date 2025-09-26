24dfbd02acbab6535b6060f399cb5a3b5f7156d0
David Felipe Osorio Escobar <dfoe@Macbook-Air-de-Diego.local>
2025-09-26 17:11:55 -0500

    actualizacion

---
+++ js/carrito.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",() => {
+const botonComprar = document.querySelectorAll(".comprar");
+const carrito = document.getElementById("carrito");
+const contador = document.getElementById("contador");
+
+let cantidad = 0;
+
+botonComprar.forEach(boton => {
+    boton.addEventListener("click", () => {
+        cantidad++;
+    contador.textContent = cantidad;
+
+    carrito.classList.add("mostrar");
+
+
+});
+});
+});
